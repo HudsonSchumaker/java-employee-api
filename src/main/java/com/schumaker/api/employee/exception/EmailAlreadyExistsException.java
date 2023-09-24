1c4b7a97452a8d9351a0e9d7d2d1e033dbde5e2e
HudsonSchumaker <hudson.schumaker@me.com>
2023-09-24 20:35:32 +0200

    fix grammar

--- src/main/java/com/schumaker/api/employee/exception/EmailAlreadyExistsException.java
+++ src/main/java/com/schumaker/api/employee/exception/EmailAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package com.schumaker.api.employee.exception;
 
 public class EmailAlreadyExistsException extends RuntimeException {
-    private static final String MESSAGE = "Email already in exists";
+    private static final String MESSAGE = "Email already exists";
 
     public EmailAlreadyExistsException() {
         super(MESSAGE);
